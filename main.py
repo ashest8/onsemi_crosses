48197fb75dc311df5609e91611fa0700518d2545
ashest8 <78396455+ashest8@users.noreply.github.com>
2022-11-25 20:37:31 -0800

    input & all columns
    shows all columns of a row based on input from user

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import pandas as pd
 
+cross = input("What competitor part do you wish to cross?\n")
+
+pd.set_option('display.max_columns', None)
+
 database = pd.read_csv("datasource/CrossReferenceReport_07_26_2022.csv", sep = ',')
-print(database[database["competitor_part"]=="LM5070SD-80"])
+#database = pd.DataFrame("datasource/CrossReferenceReport_07_26_2022.csv", sep = ',')
+
+print(database[database["competitor_part"]==cross])
